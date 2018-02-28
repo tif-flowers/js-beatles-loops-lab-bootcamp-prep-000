423a1501b9c82f50ed1789cdd966ff9a6c55ceef
Tif Flowers <tif@gentlebull.com>
2018-02-28 13:42:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian","He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
-function johnLennonFacts() {
-  var array = [];
+function johnLennonFacts(array) {
   var i = 0;
   while (i < facts.length) {array.push(facts[i] +"!!!"); i++; }
   return array;
